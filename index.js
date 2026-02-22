8da0ebb2946d3f30a23b78980143d3e9efba091b
wilfredojsr <wilfredojsr2@gmail.com>
2026-02-22 14:25:10 -0300

    Initial commit publishing to npm first time

--- index.js
+++
@@ -1,2 +0,0 @@
-export { Application } from './src/commons/application';
-export { Routes, Route, Body, Query, Param } from './src/commons/decorators/routes-register';
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { Application } from './commons/application';
+export { Routes, Route, Body, Query, Param } from './commons/decorators/routes-register';
