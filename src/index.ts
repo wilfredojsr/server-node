184f560440ec0c8e344680cdf030a7fe3808160f
wilfredojsr <wilfredojsr2@gmail.com>
2026-02-25 11:22:01 -0300

    feat: Add validators

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { Application } from './commons/application';
 export { Routes, Route, Body, Query, Param, Req, Res } from './commons/decorators/routes-register';
+export { Command, Type } from './commons/decorators/validator/validator.decorator';
+export { Match } from './commons/decorators/validator/match.decorator';
